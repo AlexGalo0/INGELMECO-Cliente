2a4938be050e7c1495310e29845157a4cffc7b33
Alex Galo <alexgalo246@gmail.com>
2023-10-19 20:54:44 -0600

    correccion navbar

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,10 +3,10 @@ import DisplayData from "./svgs/DisplayData";
 const Main: React.FC = () => {
   return (
     <div>
-      <div className="bg-[#1E1E1E] flex justify-evenly p-5 pt-8">
-        <div className="w-1/2 mr-2">
+      <div className="bg-[#1E1E1E] p-5 pt-8 lg:flex justify-evenly"> {/* Utilizamos clases de flexibilidad para distribución */}
+        <div className="w-full lg:w-1/2 mr-2"> {/* Ajustamos el ancho para pantallas grandes */}
           <h2 className="text-[#048C88] text-4xl font-bold">Historia</h2>
-          <p className="text-white pt-5 max-w-[500px] text-lg ">
+          <p className="text-white pt-5 max-w-[500px] text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
             accusamus impedit neque velit, culpa similique delectus minima,
             totam, eaque dicta debitis ducimus? Quas ipsa eum, sequi quisquam
@@ -17,29 +17,24 @@ const Main: React.FC = () => {
           </p>
         </div>
         {/* Imagen SVG */}
-        <div className="">
+        <div className="lg:ml-4"> {/* Añadimos margen izquierdo para pantallas grandes */}
           <DisplayData />
         </div>
       </div>
 
-      {/* Seccion de los valores */}
-
-      <div className="bg-[#fdfdfd]  justify-evenly p-5 pt-8">
-        <div className="">
+      {/* Sección de los valores */}
+      <div className="bg-[#fdfdfd] pt-3 text-center">
+        <div className="mx-auto max-w-full lg:max-w-[680px]"> {/* Ajustamos el ancho máximo para pantallas grandes */}
           <h2 className="text-[#202020] text-2xl font-bold">
             Nuestros Valores
           </h2>
-          <p className="text-dark pt-5  text-lg ">
+          <p className="text-dark pt-3 text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
             accusamus impedit neque velit, culpa similique delectus minima,
             totam, eaque dicta debitis ducimus? Quas ipsa eum, sequi quisquam
-            consectetur rem mollitia. Accusamus fugit corporis, iste, ratione,
-            omnis laborum repellat nobis architecto obcaecati explicabo quis?
-            Molestiae error voluptatibus quam temporibus dolor mollitia, odit
-            omnis cumque quaerat sequi eos dolore aperiam aliquam et?
+            consectetur rem mollitia.
           </p>
         </div>
-        {/* Imagen SVG */}
       </div>
     </div>
   );
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,81 +1,91 @@
-"use client";
-
+"use client"
 import Image from "next/image";
 import React, { useState } from "react";
 
 const Navbar: React.FC = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-  
-    return (
-      <nav className="bg-[#1E1E1E] min-w-[350px]">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <div className="flex pt-2">
-            <Image
-              alt="Logo INGELMECO"
-              src="/images/logo.png"
-              width={240}
-              height={51}
-            />
-          </div>
-  
-          <div className="space-x-8 hidden w-full md:block md:w-auto">
-            <a href="#" className="text-white font-bold lg:text-2xl sm:text-1xl">
-              Contáctanos
-            </a>
-            <a href="#" className="text-white font-bold lg:text-2xl">
-              Categorias
-            </a>
-            <a href="#" className="text-white font-bold lg:text-2xl">
-              Productos
-            </a>
-            <a href="#" className="text-white font-bold lg:text-2xl">
-              Sobre Nosotros
-            </a>
-          </div>
-  
-          <button
-            type="button"
-            className="ml-auto inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-  
-        {menuOpen && (
-  <div className="fixed top-0 right-0 h-screen w-3/5 bg-opacity-80 bg-[#1E1E1E] backdrop-blur">
-    <div className="h-screen flex flex-col justify-start p-4">
-      <a href="#" className="text-white font-bold text-1xl my-4">
-        Contáctanos
-      </a>
-      <a href="#" className="text-white font-bold text-1xl my-4">
-        Categorias
-      </a>
-      <a href="#" className="text-white font-bold text-1xl my-4">
-        Productos
-      </a>
-      <a href="#" className="text-white font-bold text-1xl my-4">
-        Sobre Nosotros
-      </a>
-    </div>
-  </div>
-)}
-      </nav>
-    );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
+  return (
+    <nav className="bg-[#1E1E1E] min-w-[350px]">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex pt-2">
+          <Image
+            alt="Logo INGELMECO"
+            src="/images/logo.png"
+            width={240}
+            height={51}
+          />
+        </div>
+
+        <div className="space-x-8 hidden w-full md:block md:w-auto">
+          <a href="#" className="text-white font-bold lg:text-2xl sm:text-1xl">
+            Contáctanos
+          </a>
+          <a href="#" className="text-white font-bold lg:text-2xl">
+            Categorias
+          </a>
+          <a href="#" className="text-white font-bold lg:text-2xl">
+            Productos
+          </a>
+          <a href="#" className="text-white font-bold lg:text-2xl">
+            Sobre Nosotros
+          </a>
+        </div>
+
+        <button
+          type="button"
+          className="ml-auto inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {menuOpen && (
+        <div className="fixed top-0 right-0 h-screen w-3/5 bg-opacity-80 bg-[#1E1E1E] backdrop-blur">
+          <div className="h-screen flex flex-col justify-start p-4">
+            <a href="#" className="text-white font-bold text-1xl my-4">
+              Contáctanos
+            </a>
+            <a href="#" className="text-white font-bold text-1xl my-4">
+              Categorias
+            </a>
+            <a href="#" className="text-white font-bold text-1xl my-4">
+              Productos
+            </a>
+            <a href="#" className="text-white font-bold text-1xl my-4">
+              Sobre Nosotros
+            </a>
+            <button
+              onClick={toggleMenu}
+              className="text-white font-bold text-1xl my-4 hover:underline focus:outline-none"
+            >
+              Cerrar Menú
+            </button>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
 export default Navbar;
+

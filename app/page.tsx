f1db77b6da13d7a9273fc5b2447d9147c3e6e520
Alex Galo <alexgalo246@gmail.com>
2023-10-22 03:50:01 -0600

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import  Main  from "./components/Main";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 const Conocenos: React.FC = () => {
   return (
     <div className="">
       <Navbar />
       <Main />
+     
+        <Footer />
+    
     </div>
   );
 };

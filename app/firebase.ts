259d0974d69467772114e89facfc134a6274f165
Alex Galo <alexgalo246@gmail.com>
2023-10-31 17:44:24 -0600

    avance en producto

---
+++ app/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTCLkNBXmX9ibA7_ndC7R0h77RKXRZfyU",
+  authDomain: "test-admin-app-1cb81.firebaseapp.com",
+  projectId: "test-admin-app-1cb81",
+  storageBucket: "test-admin-app-1cb81.appspot.com",
+  messagingSenderId: "832034090698",
+  appId: "1:832034090698:web:974505253806289bdb8e33",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- app/productos/[productosId]/page.tsx
+++ app/productos/[productosId]/page.tsx
@@ -1,12 +1,30 @@
-"use client"
-const Producto= (params:any) => {
-    const { params : {productosId} } = params;
-    console.log(productosId);
-    return (
-      <div className="">
-      <h1>Producto Especifico</h1>
-      </div>
-    );
+"use client";
+
+import { useEffect } from "react";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+const Producto = (params: any) => {
+  const {
+    params: { productosId },
+  } = params;
+  const fetchProducts = async () => {
+    const docRef = doc(db, "productos", productosId);
+    try {
+      const querySnapshot = await getDoc(docRef);
+      console.log(querySnapshot.data());
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
-  export default Producto;
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="">
+      <h1>Producto Especifico {productosId}</h1>
+    </div>
+  );
+};
+
+export default Producto;

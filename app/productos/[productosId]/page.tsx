4fcb1c088a71e5c0015d1b465109526ac852fe74
Alex Galo <alexgalo246@gmail.com>
2023-11-01 08:47:15 -0600

    pagina de producto especifico

--- app/productos/[productosId]/page.tsx
+++ app/productos/[productosId]/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import { useEffect } from "react";
-import { collection, doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase";
+import Image from "next/image";
+
+type Producto = {
+  nombreProducto: string;
+  categoriaProducto: string;
+  descripcionProducto: string;
+  marcaProducto: string;
+  subcategoriaProducto: string;
+  urlImagen: string;
+  urlImagenSecundaria: string;
+};
+
+
+
+
 const Producto = (params: any) => {
   const {
     params: { productosId },
   } = params;
+  const [producto, setProducto] = useState<Producto>({} as Producto);
   const fetchProducts = async () => {
     const docRef = doc(db, "productos", productosId);
     try {
       const querySnapshot = await getDoc(docRef);
-      console.log(querySnapshot.data());
+      setProducto(querySnapshot.data());
     } catch (error) {
       console.log(error);
     }
@@ -22,7 +38,13 @@ const Producto = (params: any) => {
 
   return (
     <div className="">
-      <h1>Producto Especifico {productosId}</h1>
+      <h1>Nombre: {producto.nombreProducto}</h1>
+      <h1>Categoria: {producto.categoriaProducto}</h1>
+      <h1>Descripcion: {producto.descripcionProducto}</h1>
+      <h1>Marca: {producto.marcaProducto}</h1>
+      <h1>SubCategoria: {producto.subcategoriaProducto}</h1>
+      <Image src={producto.urlImagen} alt="" width={100} height={100} />
+      <Image src={producto.urlImagenSecundaria} alt="" width={100} height={100} />
     </div>
   );
 };

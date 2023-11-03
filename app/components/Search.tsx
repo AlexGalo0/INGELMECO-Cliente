dc50f949806e669ff92731da035986593032a891
Alex Galo <alexgalo246@gmail.com>
2023-11-02 21:30:31 -0600

    añadiendo busqueda de todo tipo

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -8,7 +8,7 @@ import {
   SearchBox,
   Hits,
   Pagination,
-  Highlight,
+  RefinementList,
 } from "react-instantsearch";
 
 const APPLICATION_ID = "8UD7UMU1WD";
@@ -25,7 +25,6 @@ type HitType = {
 };
 function Hit({ hit }: { hit: HitType }) {
   return (
-    
     <div className="bg-[#1E1E1E] rounded-lg shadow-md p-4 mb-4 text-white max-w-sm ">
       <div className="flex justify-center">
         <Image src={hit.urlImagen} alt="texto" width="200" height="200" />
@@ -36,9 +35,9 @@ function Hit({ hit }: { hit: HitType }) {
       <p className="text-sm">Marca: {hit.marcaProducto}</p>
       <div className="flex justify-center">
         <Link href={`/productos/${hit.objectID}`}>
-        <button className="bg-[#048C88]  p-2 rounded-md transition-all duration-300 hover:scale-105 ">
-          Ver Producto
-        </button>
+          <button className="bg-[#048C88]  p-2 rounded-md transition-all duration-300 hover:scale-105 ">
+            Ver Producto
+          </button>
         </Link>
       </div>
     </div>
@@ -51,13 +50,19 @@ export default function Search() {
       searchClient={searchClient}
       indexName="ingelmeco_productos"
       future={{ preserveSharedStateOnUnmount: true }}
-      
     >
       <SearchBox className="search-bar" />
+      <div className="bg-red-500">
+        <RefinementList attribute="marcaProducto" />
+      </div>
+      <div className="bg-stone-100">
+      <RefinementList attribute="categoriaProducto" />
+      </div>
       <div className="flex justify-center pt-8">
         <Hits hitComponent={Hit} className="" />
       </div>
-      <div>
+
+      <div className="pagination">
         <Pagination showFirst={true} />
       </div>
     </InstantSearch>

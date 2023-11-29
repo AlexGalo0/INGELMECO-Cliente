c1ce412911da9f2de8d9cd24c95d5c9ceeafc727
Alex Galo <alexgalo246@gmail.com>
2023-11-28 20:28:06 -0600

    avance

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -17,7 +17,12 @@ const Inicio: React.FC = () => {
           ></Image>
         </div>
         <div className="w-full lg:w-1/2 mr-2 lg:mx-auto">
-          <h1 className="pt-4">INGELMECO</h1>
+          <div className="flex justify-center items-center">
+            <h1 className="pt-4 text-[#048C88] text-4xl font-bold">
+              Somos Ingelmeco
+            </h1>
+          </div>
+
           <div className="pt-6">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
@@ -31,11 +36,11 @@ const Inicio: React.FC = () => {
               quasi velit sunt corrupti asperiores beatae, atque magni.
             </p>
           </div>
-          <div className="pt-12">
+          <div className="pt-6 flex justify-center items-center">
             <Link href="/">
               <button
                 type="button"
-                className="text-white bg-[#048C88] hover:bg-[#057E79] focus:ring-4 focus:outline-none focus:ring-[#057E79] font-medium rounded-lg text-base px-5 py-2.5 text-center inline-flex items-center dark:bg-[#057E79] dark:hover:bg-[#046C68] dark:focus:ring-[#046C68]"
+                className="text-white bg-[#048C88] hover:bg-[#057E79] focus:ring-4 focus:outline-none focus:ring-[#057E79] font-medium rounded-lg text-base px-5 py-2.5 text-center inline-flex items-center dark:bg-[#057E79] dark:hover:bg-[#046C68] dark:focus:ring-[#046C68] transition-all duration-300 hover:scale-110"
               >
                 Nuestro Catálogo
                 <svg
@@ -59,25 +64,29 @@ const Inicio: React.FC = () => {
         </div>
       </div>
 
-      {/* Sección de los valores */}
+      {/* Sección de productos de SIEMENS */}
       <div className="bg-[#202020] pt-3 text-center pb-7">
-        <div className="mx-auto max-w-full lg:max-w-[680px]">
+        <div className="mx-auto max-w-full lg:max-w-[800px] animate-fade-left animate-once animate-ease-linear">
           <h2 className=" text-2xl font-bold text-[#eee1e1] ">
-            Nuestra especialidad , Productos Siemens
+            SIEMENS Nuestra Especialidad
           </h2>
           <p className="text-dark p-4 text-lg text-[#eee1e1]">
-            Como distribuidores oficiales de SIEMENS , puedes encontrar mucha
-            variedad de productos{" "}
+            Uno de los principales orgullos para INGELMECO es ser los
+            distribuidores oficiales de SIEMENS en Honduras , por lo cual
+            contamos con una amplia gama de produtos de la marca.
           </p>
         </div>
-        <div className="grid gap-10 px-14 2xl:gap-36 lg:gap-20 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-10 px-14 2xl:gap-36 lg:gap-20 sm:grid-cols-2 lg:grid-cols-3 animate-fade-right animate-once animate-ease-linear">
+          <div>
+
           <Card
             backgroundColor="#1E1E1E"
             imageSrc="/images/exito.webp"
             textColor="#FFFFFF"
             title="Producto 1"
             description={"Producto 1"}
-          />
+            />
+            </div>
           <Card
             backgroundColor="#1E1E1E"
             imageSrc="/images/exito.webp"
@@ -95,59 +104,59 @@ const Inicio: React.FC = () => {
         </div>
       </div>
 
-      {/* Sección de los Servicios */}
-      <div className="bg-[#202020] pt-3 text-center pb-7 border-b-[30px]">
+      {/* Sección de las Marcas */}
+      <div className="bg-[#eee1e1] pt-3 text-center pb-7 border-b-[30px] ">
         <div className="mx-auto max-w-full lg:max-w-[680px]">
-          <h2 className="text-white text-2xl font-bold px-3">
+          <h2 className="text-[#1E1E1E] text-2xl font-bold px-3">
             Nuestras Marcas
           </h2>
           <p className="text-white p-4 text-lg"></p>
         </div>
-        <div className="grid gap-10 px-14 2xl:gap-36 lg:gap-20 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-10 px-14 2xl:gap-36 lg:gap-20 sm:grid-cols-2 lg:grid-cols-3 ">
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
             description={""}
           />
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
             description={""}
           />
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
             description={""}
           />
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
             description={""}
           />
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
             description={""}
           />{" "}
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
             description={""}
           />{" "}
           <Card
-            backgroundColor="#DDDDDD"
+            backgroundColor="#1E1E1E"
             imageSrc="/images/edit-tools.webp"
             textColor="#202020"
             title="ABB"
